f0fbea6dfadd7f9081e470f4bf9e544bef1eaddd
Pragna_Kongara <147132255+PragnaKongara@users.noreply.github.com>
2024-12-13 10:52:03 +0530

    Update demo.java
    updated demo.java

--- demo.java
+++ demo.java
@@ -1,10 +1,7 @@
 import java.util.*;
 public class demo{
 	public static void main(String[] args){
-		int a=8;
-		int b=1;
-		int c=a+b;
-		System.out.println("sum:"+ c);
+		System.out.println("Good Mornig");
 	}
 }
 	
